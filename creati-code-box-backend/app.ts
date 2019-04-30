83082226e4f2bbb516e4d6be5b9c5a12593c7694
creaticoding <creaticoding@gmail.com>
2019-05-01 04:04:25 +0900

    add hello world watch mode

--- creati-code-box-backend/app.ts
+++ creati-code-box-backend/app.ts
@@ -1,1 +1,12 @@
-console.log('hello world!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!!');
+});
+
+app.listen(3000, function() {
+  console.log('Start listening on port 3000!');
+  console.log('http://localhost:3000');
+});
