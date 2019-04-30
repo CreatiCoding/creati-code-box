be4ef1fbb085a8a6786951b020cb249023a7ab5f
creaticoding <creaticoding@gmail.com>
2019-05-01 05:58:29 +0900

    add alias path and router 분리

--- creati-code-box-backend/src/app.ts
+++ creati-code-box-backend/src/app.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import router from '@/router';
 
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!!');
-});
+app.use('/', router);
 
 app.listen(3000, function() {
   console.log('Start listening on port 3000!');
