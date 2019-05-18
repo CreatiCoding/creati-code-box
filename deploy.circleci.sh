446e4d14e5d6017594a8d4b08e616924ee7304ad
creaticoding <creaticoding@gmail.com>
2019-05-18 21:14:18 +0900

    fix environment

--- deploy.circleci.sh
+++ deploy.circleci.sh
@@ -1,7 +1,7 @@
 ls -al ~/.ssh
 pwd
 if [ "$CIRCLE_BRANCH" = "deploy/master/frontend" ]; then
-    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/id_creco2019 -o StrictHostKeyChecking=no <<'ENDSSH'
+    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no <<'ENDSSH'
         sudo pgrep -f nuxt | xargs kill -9
         sudo rm -rf master_frontend
         mkdir master_frontend
@@ -11,7 +11,7 @@ if [ "$CIRCLE_BRANCH" = "deploy/master/frontend" ]; then
         nohup ./deploy.sh </dev/null >/dev/null 2>&1 &
 ENDSSH
 elif [ "$CIRCLE_BRANCH" = "deploy/develop/frontend" ]; then
-    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/id_creco2019 -o StrictHostKeyChecking=no <<'ENDSSH'
+    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no <<'ENDSSH'
         sudo pgrep -f nuxt | xargs kill -9
         sudo rm -rf develop_frontend
         mkdir develop_frontend
