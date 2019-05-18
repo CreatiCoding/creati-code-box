d2c44d4027632f9219e45c0864156ef87e6a8514
creaticoding <creaticoding@gmail.com>
2019-05-18 21:06:59 +0900

    fix shell file

--- deploy.circleci.sh
+++ deploy.circleci.sh
@@ -7,7 +7,7 @@ if [ "$CIRCLE_BRANCH" = "deploy/master/frontend" ]; then
         git clone --branch deploy/master/frontend https://github.com/CreatiCoding/creati-code-box
         cd creati-code-box
         nohup ./deploy.sh </dev/null >/dev/null 2>&1 &
-ENDSSH;
+ENDSSH
 elif [ "$CIRCLE_BRANCH" = "deploy/develop/frontend" ]; then
     ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/id_creco2019 -o StrictHostKeyChecking=no <<'ENDSSH'
         sudo pgrep -f nuxt | xargs kill -9
@@ -17,5 +17,5 @@ elif [ "$CIRCLE_BRANCH" = "deploy/develop/frontend" ]; then
         git clone --branch deploy/develop/frontend https://github.com/CreatiCoding/creati-code-box
         cd creati-code-box
         nohup ./deploy.sh </dev/null >/dev/null 2>&1 &
-ENDSSH;
+ENDSSH
 fi
