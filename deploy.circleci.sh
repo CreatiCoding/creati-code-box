e9a64e4990a6de96d84a47b7e988a3fdb4e2077c
creaticoding <creaticoding@gmail.com>
2019-05-18 21:20:23 +0900

    logging

--- deploy.circleci.sh
+++ deploy.circleci.sh
@@ -1,23 +1,8 @@
 ls -al ~/.ssh
 pwd
 if [ "$CIRCLE_BRANCH" = "deploy/master/frontend" ]; then
-    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -T -o StrictHostKeyChecking=no <<'ENDSSH'
-        sudo pgrep -f nuxt | xargs kill -9
-        sudo rm -rf master_frontend
-        mkdir master_frontend
-        cd master_frontend
-        git clone --branch deploy/master/frontend https://github.com/CreatiCoding/creati-code-box
-        cd creati-code-box
-        nohup ./deploy.sh </dev/null >/dev/null 2>&1 &
+    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/master.frontend.sh
 ENDSSH
 elif [ "$CIRCLE_BRANCH" = "deploy/develop/frontend" ]; then
-    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -T -o StrictHostKeyChecking=no <<'ENDSSH'
-        sudo pgrep -f nuxt | xargs kill -9
-        sudo rm -rf develop_frontend
-        mkdir develop_frontend
-        cd develop_frontend
-        git clone --branch deploy/develop/frontend https://github.com/CreatiCoding/creati-code-box
-        cd creati-code-box
-        nohup ./deploy.sh </dev/null >/dev/null 2>&1 &
-ENDSSH
+    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/develop.frontend.sh
 fi
