e869fbdb1ad3fb41215122048cc6ca30b01c9a00
creaticoding <creaticoding@gmail.com>
2019-05-19 03:29:56 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,23 @@ DEPLOY_MASTER_BACKEND='deploy/master/backend'
 echo $PWD
 
 if [ "$BRANCH" = "$DEPLOY_DEVELOP_FRONTEND" ]; then
+  cd ../..
+  pgrep -f develop.*frontend | xargs --no-run-if-empty kill -9
+  sudo rm -rf develop_frontend
+  mv develop_frontend_new develop_frontend
+  cd develop_frontend
+  cd creati-code-box
   cd creati-code-box-frontend 
   npm i
   npm run start:d
   exit 1;
 elif [ "$BRANCH" = "$DEPLOY_MASTER_FRONTEND" ]; then
+  cd ../..
+  pgrep -f master.*frontend | xargs --no-run-if-empty kill -9
+  sudo rm -rf master_frontend
+  mv master_frontend_new master_frontend
+  cd master_frontend
+  cd creati-code-box
   cd creati-code-box-frontend 
   npm i
   npm run start:p
