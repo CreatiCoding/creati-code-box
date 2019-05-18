4a1f10549968f431473748fc177951d191bd9685
creaticoding <creaticoding@gmail.com>
2019-05-18 21:34:26 +0900

    modify ssh key path

--- deploy.circleci.sh
+++ deploy.circleci.sh
@@ -1,8 +1,8 @@
-ls -al ~/.ssh
+ls -al ~/.ssh/$SSH_FILE
 pwd
 if [ "$CIRCLE_BRANCH" = "deploy/master/frontend" ]; then
     ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/master.frontend.sh 2>&1
 ENDSSH
 elif [ "$CIRCLE_BRANCH" = "deploy/develop/frontend" ]; then
-    ssh ubuntu@$SERVER_HOSTNAME -i ~/ppk/creco/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/develop.frontend.sh 2>&1
+    ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/develop.frontend.sh 2>&1
 fi
