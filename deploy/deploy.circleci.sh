0c88e1d14860ef3cd98a36872c5c7b1d87639b90
creaticoding <creaticoding@gmail.com>
2019-05-18 21:40:00 +0900

    debug

--- deploy/deploy.circleci.sh
+++ deploy/deploy.circleci.sh
@@ -1,8 +1,9 @@
 ls -al ~/.ssh/$SSH_FILE
 pwd
 if [ "$CIRCLE_BRANCH" = "deploy/master/frontend" ]; then
+    echo 'master'
     ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/master.frontend.sh 2>&1
-ENDSSH
 elif [ "$CIRCLE_BRANCH" = "deploy/develop/frontend" ]; then
+    echo 'develop'
     ssh ubuntu@$SERVER_HOSTNAME -i ~/.ssh/$SSH_FILE -o StrictHostKeyChecking=no < ./deploy/develop.frontend.sh 2>&1
 fi
